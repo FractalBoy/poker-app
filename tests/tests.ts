35c787e7dff8330b57649ec2fa6d8e8b0e7384d5
Marc Reisner <reisner.marc@gmail.com>
2020-04-28 21:45:32 -0500

    Finish tests on straights, flushes, and straight flushes

--- tests/tests.ts
+++ tests/tests.ts
@@ -6,6 +6,49 @@ import Rank from '../poker/Rank';
 import Suit from '../poker/Suit';
 
 describe('Hands', function () {
+    describe('possibleStraightFlushes', function () {
+        it('should return an empty array when there are no straight flushes', function () {
+            const hands = new Hands([
+                new Card(Rank.Ace, Suit.Diamonds),
+                new Card(Rank.Queen, Suit.Diamonds),
+                new Card(Rank.Ten, Suit.Spades),
+                new Card(Rank.King, Suit.Spades),
+                new Card(Rank.Six, Suit.Clubs),
+                new Card(Rank.Jack, Suit.Clubs),
+                new Card(Rank.Three, Suit.Hearts),
+                new Card(Rank.Two, Suit.Hearts)
+            ]);
+
+            const straightFlushes = hands.possibleStraightFlushes();
+            straightFlushes.should.be.an.Array();
+            straightFlushes.should.have.lengthOf(0);
+        });
+
+        it('should return a single straight flush when there is one straight flush', function () {
+            const hands = new Hands([
+                new Card(Rank.Ace, Suit.Diamonds),
+                new Card(Rank.King, Suit.Diamonds),
+                new Card(Rank.Queen, Suit.Diamonds),
+                new Card(Rank.Jack, Suit.Diamonds),
+                new Card(Rank.Ten, Suit.Diamonds),
+                new Card(Rank.Four, Suit.Clubs),
+                new Card(Rank.Three, Suit.Hearts),
+                new Card(Rank.Two, Suit.Hearts)
+            ]);
+
+            const straightFlushes = hands.possibleStraightFlushes();
+            straightFlushes.should.be.an.Array();
+            straightFlushes.should.have.lengthOf(1);
+            straightFlushes[0].cards.should.deepEqual([
+                new Card(Rank.Ace, Suit.Diamonds),
+                new Card(Rank.King, Suit.Diamonds),
+                new Card(Rank.Queen, Suit.Diamonds),
+                new Card(Rank.Jack, Suit.Diamonds),
+                new Card(Rank.Ten, Suit.Diamonds)
+            ]);
+        });
+    });
+
     describe('possibleStraights', function () {
         it('should return an empty array when there are no straights', function () {
             const hands = new Hands([
